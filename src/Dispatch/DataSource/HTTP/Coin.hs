eccc5ce57a922cc2980ca6b71cbe63430310c237
Lupino <lmjubuntu@gmail.com>
2016-11-28 21:46:04 +0800

    Fixed. saveCoin

--- src/Dispatch/DataSource/HTTP/Coin.hs
+++ src/Dispatch/DataSource/HTTP/Coin.hs
@@ -16,10 +16,19 @@ import           Network.Wreq
 -- post "/api/coins/:name/"
 saveCoin :: UserName -> Coin -> Gateway -> IO (Either ErrResult ScoreResult)
 saveCoin n c gw =
-  responseEither $ asJSON =<< postWith opts uri (toJSON c)
+  responseEither $ asJSON =<< postWith opts uri [ "score" := score
+                                                , "type" := show tp
+                                                , "desc" := desc
+                                                , "created_at" := ct
+                                                ]
 
   where opts = getOptions gw
         uri = concat [ getGWUri gw, "/api/coins/", unpack n, "/" ]
+
+        score = coinScore c
+        tp = coinType c
+        desc = coinDesc c
+        ct = coinCreatedAt c
 
 -- get "/api/coins/:name/score/"
 getCoinScore :: UserName -> Gateway -> IO (Either ErrResult ScoreResult)

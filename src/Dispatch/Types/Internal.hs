4ab7c8b8ca2a0ceaad6d963b288f008a8c1623ff
Lupino <lmjubuntu@gmail.com>
2017-02-15 13:39:51 +0800

    update type gateway

--- src/Dispatch/Types/Internal.hs
+++ src/Dispatch/Types/Internal.hs
@@ -9,15 +9,25 @@ module Dispatch.Types.Internal
   , CreatedAt
   ) where
 
-import           Data.Aeson (FromJSON (..), ToJSON (..), Value, object,
-                             withObject, (.:), (.=))
+import           Data.Aeson (FromJSON (..), Value, withObject, (.!=), (.:),
+                             (.:?), (.=))
 import           Data.Int   (Int64)
 
-data Gateway = Gateway { getGWUri       :: String
-                       , getGWAppKey    :: String
-                       , getGWAppSecret :: String
+data Gateway = Gateway { getGWUri        :: String
+                       , getGWAppKey     :: String
+                       , getGWAppSecret  :: String
+                       , getGWNumThreads :: Int
+                       -- numThreads of fetch async for haxl
                        }
   deriving (Show)
+
+instance FromJSON Gateway where
+  parseJSON = withObject "Gateway" $ \o -> do
+    getGWUri        <- o .:? "host"       .!= "https://gw.huabot.com"
+    getGWAppKey     <- o .:  "key"
+    getGWAppSecret  <- o .:  "secret"
+    getGWNumThreads <- o .:  "numThreads" .!= 1
+    return Gateway{..}
 
 class AppEnv a where
   gateway :: a -> String -> Gateway
